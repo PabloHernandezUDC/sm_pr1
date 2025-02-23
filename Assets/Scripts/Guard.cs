8a693a79242b2d769e460cef62796d954f503afd
PabloHernandezUDC <125274113+PabloHernandezUDC@users.noreply.github.com>
2025-02-23 11:32:48 +0100

    representación visual de la visión de los guardias

--- Assets/Scripts/Guard.cs
+++ Assets/Scripts/Guard.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
@@ -6,49 +7,133 @@ using UnityEngine.AI;
 
 public class Guard : MonoBehaviour
 {
+    public float radius;
+    [Range(0, 360)]
+    public float angle;
+
+    public GameObject thief;
+
+    public LayerMask targetMask;
+    public LayerMask obstructionMask;
+
+    bool canSeePlayer;
+    LineRenderer line;
+
     Transform trans;
     NavMeshAgent agent;
-    public List<Transform> targets;
-    public Thief thief;
+    public List<Transform> patrol_points;
 
     int current_target;
     int n_of_targets;
 
-    public bool patrolling;
-    public float chase_time;
+    bool patrolling;
+    float chase_time;
     public int max_chase_time;
 
-    // Start is called before the first frame update
-    void Start()
+
+    private void Start()
     {
+        StartCoroutine(FOVRoutine());
+
+        line = GetComponent<LineRenderer>();
+        line.startWidth = 0.2f;
+        line.endWidth = 0.8f;
+
         trans = GetComponent<Transform>();
         agent = GetComponent<NavMeshAgent>();
-        agent.destination = targets.First().position;
+        agent.destination = patrol_points.First().position;
 
         current_target = 0;
-        n_of_targets = targets.Count;
+        n_of_targets = patrol_points.Count;
         patrolling = true;
         chase_time = 0;
+
     }
 
-    // Update is called once per frame
+    private IEnumerator FOVRoutine()
+    {
+        WaitForSeconds wait = new WaitForSeconds(0.016f);
+
+        while (true)
+        {
+            yield return wait;
+            FieldOfViewCheck();
+        }
+    }
+
+    private void FieldOfViewCheck()
+    {
+        line.enabled = false;
+        // comprobamos si hay un jugador en nuestro radio
+        Collider[] rangeChecks = Physics.OverlapSphere(transform.position, radius, targetMask);
+
+        if (rangeChecks.Length != 0)
+        {
+            // cogemos su posición y la distancia hasta él
+            Transform target = rangeChecks[0].transform;
+            Vector3 directionToTarget = (target.position - transform.position).normalized;
+
+            // si está en nuestro "campo de vista" y la línea de visión no está obstruida, significa que podemos verlo
+            if (Vector3.Angle(transform.forward, directionToTarget) < angle / 2)
+            {
+                float distanceToTarget = Vector3.Distance(transform.position, target.position);
+                
+                Ray ray = new Ray(transform.position, directionToTarget);
+                RaycastHit hit;
+
+                // dibujamos el rayo
+                var points = new Vector3[2];
+                points[0] = transform.position;
+
+                if (Physics.Raycast(ray, out hit, distanceToTarget, obstructionMask))
+                {
+                    // la vista está obstruida y solo dibujamos hasta la pared
+                    canSeePlayer = false;
+                    points[1] = hit.point;
+                }
+                else
+                {
+                    // lo vemos sin obstrucciones y dibujamos hasta el jugador
+                    canSeePlayer = true;
+                    patrolling = false;
+                    chase_time = 0;
+                    points[1] = target.position;
+                }
+
+                // "activamos" la línea para dibujarla y le pasamos las coordenadas
+                line.enabled = true;
+                line.SetPositions(points);
+            }
+            else
+                canSeePlayer = false;
+        }
+        else if (canSeePlayer) // si lo hemos dejado de ver, cambiamos el valor
+            canSeePlayer = false;
+    }
+
     void Update()
     {
         if (patrolling)
         {
+            // si hemos llegado, pasamos al siguiente
             if (trans.position.x == agent.destination.x && trans.position.z == agent.destination.z)
             {
                 current_target++;
-                agent.destination = targets[current_target  % n_of_targets].position;
-            }            
+                agent.destination = patrol_points[current_target % n_of_targets].position;
+            }
         }
         else
         {
-            agent.destination = thief.transform.position;
-            chase_time += Time.deltaTime;
+            // si ha pasado demasiado tiempo, volvemos a patrullar
             if (chase_time >= max_chase_time)
             {
                 patrolling = true;
+                agent.destination = patrol_points[current_target % n_of_targets].position;
+            }
+            else // si no, actualizamos la posición y seguimos persiguiendo
+            {
+                agent.destination = thief.transform.position;
+                chase_time += Time.deltaTime;
             }
         }
     }
